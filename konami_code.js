db6bae09fe2ad61dfa341c56e1f6fd5337f67fb4
Denise Hastings <dehast59@gmail.com>
2018-09-13 14:43:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,18 @@ const codes = [
 
 function init() {
   // your code here
+  document.body.addEventListener('keydown', function(event){
+    let index = 0;
+      const key = event.key;
+      if (key === codes[index]){
+        console.log(key);
+        index++;
+        if (index === codes.length) {
+          alert("Hurray!");
+          index = 0;
+        }
+      }else {
+        index = 0;
+    } 
+})
 }
